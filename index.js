42838945887ead4c10acbc4a892e855690ca1f1c
TimiMaliki <malikitimi@gmail.com>
2024-01-02 11:55:17 -0800

    omo

---
+++ index.js
@@ -0,0 +1,32 @@
+const powerBtn = document.querySelector(".bi-power")
+const semiCircle = document.querySelector(".smaller-cirlce");
+const switchBtn = document.querySelector(".switch")
+
+const sunSet = document.querySelector(".sun-set")
+const sunDown = document.querySelector(".sun-dawn")
+
+const halfMoon = document.querySelector(".half-moon");
+const fullMoon = document.querySelector(".full-moon");
+const faraway = document.querySelector(".faraway")
+
+
+
+// POWER BTN (TURN ON)
+powerBtn.addEventListener('click' , (e) =>{
+   // TOGGLE CLASS
+    faraway.classList.toggle('visibility')
+    faraway.classList.add("faraway")
+    semiCircle.classList.toggle('visibility')
+})
+
+// SUNSET 
+sunSet.addEventListener( "click" , (e) => {
+
+
+    //SWITCHING PLACES
+      sunSet.classList.add("switchPlay-SunDown")
+      sunDown.classList.add("switchPlay-FullMoon")
+      fullMoon.classList.add("switchPlay-HalfMoon")
+      halfMoon.classList.add("switchPlay-SUNSET")
+})
+
